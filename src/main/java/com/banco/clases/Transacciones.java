017e7d2393f0543c461930d65bbe7804c095c384
arwenKonne <aurelio.iempereur@gmail.com>
2022-08-29 16:20:52 -0600

    menú selección de Banco
    menú funcional, y envía correctamente los datos, además de ser reutilizable en la misma clase

--- src/main/java/com/banco/clases/Transacciones.java
+++ src/main/java/com/banco/clases/Transacciones.java
@@ -5,11 +5,63 @@ import com.banco.clases.clasesImpresion.imprimirPdf;
 import com.banco.interfaces.Imprimir;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 @NoArgsConstructor
 public class Transacciones {
+
+    private  String nombreBanco;
+    private  String direccionBanco;
+    public void menu(){
+        List<String> nombres = new ArrayList<>();
+        List<String> direccion = new ArrayList<>();
+
+        nombres.add("-----");
+        nombres.add("Banco de América Central");
+        nombres.add("Banco Agricola");
+        nombres.add("Banco Cuscatlán de El Salvador");
+        nombres.add("Banco Davivienda Salvadoreño");
+        nombres.add("Banco Promérica");
+        nombres.add("Banco Atlántida El Salvador");
+        nombres.add("Banco ABANK");
+        nombres.add("Banco Industrial El Salvador");
+        nombres.add("Banco Azul de El Salvador");
+
+        direccion.add("------");//none
+        direccion.add("55 Av. Sur, entre Alameda Roosevelt y Av. Olímpica Edif. Credomatic, San Salvador, El Salvador, C.A.");//BAC
+        direccion.add("Blvd. Constitución No.:100, San Salvador, El Salvador, C.A.");//Agricola
+        direccion.add("Edificio Pirámide Km. 10 carretera a Santa Tecla. Depto. La Libertad.");//Cuscatlán
+        direccion.add("Avenida Olímpica No.3550. San Salvador, El Salvador, C.A. Apdo. Postal No.: (0673).");//Davivienda
+        direccion.add("Edificio Promérica, Centro de Estilo de Vida La Gran Vía, entre Carretera Panamericana y Calle Chiltiupán, Antiguo Cuscatlán, La Libertad.");//Promérica
+        direccion.add("Blvd. Constitución y Primera Calle Poniente No. 3538 Col. Escalón, San Salvador.");//Atlantida
+        direccion.add("Boulevard Merliot, Urbanización Jardines de la Hacienda, Edificio Spatium Santa Fe, Lote 4-5-6 y 7, Antiguo Cuscatlán, La Libertad.");//ABANK
+        direccion.add("Avenida las Magnoleas, Boulevard el Hipódromo, No 144 Colonia, San Benito, San Salvador, C.A.");//Industrial
+        direccion.add("Alameda Manuel Enrique Araujo y Avenida Olímpica No. 3553, Colonia Escalón, San Salvador.");//Azul
+
+        Scanner na = new Scanner(System.in);
+        System.out.println("Seleccione el Banco al que pertenece su cuenta : \n" +
+                "1. Banco de América Central \n" +
+                "2. Banco Agricola \n" +
+                "3. Banco Cuscatlán de El Salvador \n" +
+                "4. Banco Davivienda Salvadoreño \n" +
+                "5. Banco Promérica \n" +
+                "6. Banco Atlántida El Salvador \n" +
+                "7. Banco ABANK \n" +
+                "8. Banco Industrial El Salvador \n" +
+                "9. Banco Azul de El Salvador");
+        int selection = na.nextInt();
+        if(selection <= 9 && selection != 0){
+            nombreBanco = nombres.get(selection);
+            direccionBanco = direccion.get(selection);
+        }else {
+            System.out.println("Seleccione una de las opciones dadas, de 1 a 9 \n" +
+                    "***********************************************************\n");
+            menu();
+        }
+    }
 
     public void retiro(Cuenta cuenta){
         double saldo;
@@ -18,25 +70,13 @@ public class Transacciones {
         saldo = sc.nextDouble();
         cuenta.saldo = cuenta.saldo -saldo;
     }
-    //Porque no usar un solo escaner ?
-    //Se repite el nombre del banco (no seria mejor tener el banco ya echo y desplegar un meno para seleccionar banco)
-    public void deposito(Cuenta cuenta){
-        Imprimir generarPDF =new imprimirPdf();
-        Imprimir imprimir = new imprimirCorreo();
 
+    public void deposito(Cuenta cuenta){
+        Imprimir generarPDF = new imprimirPdf();
+        Imprimir imprimir = new imprimirCorreo();
         double deposito;
-        String nombreBanco;
-        String direccionBanco;
-        // Datos del Banco
-        // nombre
-        Scanner na = new Scanner(System.in);
-        System.out.println("Ingrese el nombre de su banco");
-        nombreBanco = na.next();
-        //Direccion
-        Scanner da = new Scanner(System.in);
-        System.out.println("Ingrese el nombre de su banco");
-        direccionBanco = da.next();
         // instanciación
+        menu();
         Banco banco = new Banco(nombreBanco,1,direccionBanco);
         // Datos a ingresar
         Scanner sc = new Scanner(System.in);
@@ -44,7 +84,6 @@ public class Transacciones {
         deposito = sc.nextDouble();
         // Calculo del Deposito
         cuenta.saldo = cuenta.saldo +deposito;
-
         generarPDF.imprimir(cuenta,banco,deposito,"deposito");
         imprimir.imprimir(cuenta,banco,deposito,"deposito");
     }
